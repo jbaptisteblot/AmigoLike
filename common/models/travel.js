2dfec80f483b974ad05dbfc74eea1db1eb527441
Jeanba88 <admin@jeanba88.com>
2018-10-25 13:15:06 -0400

    Ajout d'un outil de recherche basique
    Modification du readme

--- common/models/travel.js
+++ common/models/travel.js
@@ -1,5 +1,33 @@
 'use strict';
 
 module.exports = function(Travel) {
+  Travel.search = function(dateDepart, lieuDepart, lieuArrivee, cb) {
+    Travel.find({'include': 'trips'}).then(function(travels) {
+      var foundTravels = [];
+      travels.forEach(function(travelObj) {
+        var travelJson = travelObj.toJSON();
+        var availableBegin = false;
+        var availableEnd = false;
+        travelJson.trips.forEach(function(trip) {
+          if (trip.beginLocation === lieuDepart)
+            availableBegin = true;
+          if (trip.endLocation === lieuArrivee)
+            availableEnd = true;
+        });
+        if (availableBegin && availableEnd) {
+          foundTravels.push(travelJson);
+        }
+      });
+      cb(null, foundTravels);
+    });
+  };
 
+  Travel.remoteMethod('search', {
+    accepts: [
+            {arg: 'dateDepart', type: 'string'},
+            {arg: 'lieuDepart', type: 'string'},
+            {arg: 'lieuArrivee', type: 'string'},
+    ],
+    returns: {arg: 'travel', type: 'travel'},
+  });
 };
